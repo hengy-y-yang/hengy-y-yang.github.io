93f1d23b683ba1bd825e55366d1fc19df820864d
BuildTools <unconfigured@null.spigotmc.org>
2021-11-19 08:16:10 -0800

    test

--- assets/js/important.js
+++ assets/js/important.js
@@ -3,7 +3,7 @@ function important() {
     let i, unimportant;
 
     for (i = 0, unimportant = 0; i < cards.length; i++) {
-      if (cards[i].tag == "important") {
+      if (cards[i].tag != "") {
         makeCard(cards[i], i - unimportant);
       } else unimportant++;
     }
